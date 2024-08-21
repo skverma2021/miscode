6bb38704ee8017481f50fc1be948b61659d0ae91
SK Verma <s.k.verma@live.in>
2024-08-21 14:56:21 +0530

    21Aug24 - 1

---
+++ frontend/src/auth/LoginCopy.jsx
@@ -0,0 +1,111 @@
+import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
+import Spinner from '../home/Spinner';
+import { jwtDecode } from 'jwt-decode';
+
+const Login = () => {
+  const [emp, setEmp] = useState({
+    eMail: '',
+    pass: '',
+  });
+
+  const [status, setStatus] = useState('');
+
+  const okSubmit = () => {
+    if (emp.eMail.length == 0) return false;
+    if (emp.pass.length == 0) return false;
+    return true;
+  };
+
+  const onValChange = (e) => {
+    setEmp({ ...emp, [e.target.name]: e.target.value });
+  };
+
+  const logInUser = async (event) => {
+    setStatus('busy');
+    event.preventDefault();
+    try {
+      const res = await axios.get(
+        `http://localhost:3000/api/emps/${emp.eMail}/${emp.pass}`
+      );
+      const decoded = jwtDecode(res.data.token);
+      console.log(decoded.eID, decoded.eName, decoded.eDepttID);
+      setStatus('success');
+    } catch (error) {
+      setStatus('error');
+      console.log(error);
+    }
+  };
+
+  if (status === 'error') {
+    return <h1 style={{ color: 'red' }}>Error Occured !</h1>;
+  }
+
+  if (status === 'busy') return <Spinner />;
+
+  return (
+    <div
+      style={{
+        marginTop: '100px',
+        height: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignContent: 'center',
+      }}
+    >
+      <form onSubmit={logInUser}>
+        <table style={{ lineHeight: '50px' }}>
+          <tbody>
+            <tr>
+              <td colSpan={2}>
+                <h2>LOGIN</h2>
+              </td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>eMail ID</td>
+              <td>
+                <input
+                  name='eMail'
+                  type='email'
+                  required
+                  value={emp.eMail || ''}
+                  onChange={(e) => {
+                    return onValChange(e);
+                  }}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>Password</td>
+              <td>
+                <input
+                  name='pass'
+                  type='password'
+                  minLength={3}
+                  required
+                  value={emp.pass || ''}
+                  onChange={(e) => {
+                    return onValChange(e);
+                  }}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td colSpan={2}>
+                <button type='submit' disabled={!okSubmit()}>
+                  Login
+                </button>
+              </td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+        <br />
+      </form>
+    </div>
+  );
+};
+
+export default Login;
